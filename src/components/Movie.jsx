63e70b37cf2f9302d3e0c7e30acdb746a8a5423b
ErwinBrgr <t.chaperon@gmail.com>
2019-09-30 14:48:33 +0200

    ajout CA films

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,29 +8,28 @@ const API_KEY = "5e02044861740f3118eb48cc66fb2274"
 
 class Movie extends Component{
 
+  state = {
+    movieDetail  : []
+  }
+ 
 
- async componentDidMount(){
+async componentDidMount(){
     const url = 'https://api.themoviedb.org/3/movie/'+ this.props.movie.id+'?api_key=5e02044861740f3118eb48cc66fb2274&language=en-US';
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({post: data.results})
+    this.setState({movieDetail: data})
     console.log(data)
   }
 
-// const ID = this.props.movie.id
-// console.log(ID)
-
-
-
   render() {
-    console.log(this.props)
-    console.log(this.props.movie.revenue)
+    // console.log(this.props)
+    console.log(this.state.movieDetail)
     return (
       <li id="individual"> 
         <img src={'https://image.tmdb.org/t/p/w500/' + this.props.movie.poster_path}/>
         <p>{this.props.movie.title}</p>
         <p>{this.props.movie.release_date}</p>
-        <p>{this.props.movie.revenue}</p>
+        <p>{this.state.movieDetail.revenue - this.state.movieDetail.budget}</p>
 
       </li>
     )
