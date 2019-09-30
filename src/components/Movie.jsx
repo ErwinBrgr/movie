18520654c9b9ce10c82f67717b5c43561c66f7af
ErwinBrgr <t.chaperon@gmail.com>
2019-09-30 14:17:48 +0200

    chargement détail films

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -9,25 +9,29 @@ const API_KEY = "5e02044861740f3118eb48cc66fb2274"
 class Movie extends Component{
 
 
- // componentDidMount(){
- //    const url = 'https://api.themoviedb.org/3/movie/{'+ this.props.movie.id +'}?api_key=' + API_KEY + '&language=en-US';
- //    const response = fetch(url);
- //    const data =  response.json();
- //    this.setState({post: data.results})
- //    console.log(data)
- //  }
+ async componentDidMount(){
+    const url = 'https://api.themoviedb.org/3/movie/'+ this.props.movie.id+'?api_key=5e02044861740f3118eb48cc66fb2274&language=en-US';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({post: data.results})
+    console.log(data)
+  }
 
-
+// const ID = this.props.movie.id
+// console.log(ID)
 
 
 
   render() {
     console.log(this.props)
+    console.log(this.props.movie.revenue)
     return (
       <li id="individual"> 
         <img src={'https://image.tmdb.org/t/p/w500/' + this.props.movie.poster_path}/>
         <p>{this.props.movie.title}</p>
-        <p>{this.props.movie.release_date}</p> 
+        <p>{this.props.movie.release_date}</p>
+        <p>{this.props.movie.revenue}</p>
+
       </li>
     )
   }
